cd091a41220b6aeac0faa83cc66933c1bf468642
Youssef Abdeltwab <Youssef.Abdeltwab@itworx.com>
2025-10-25 20:24:01 +0300

    mmm

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364c28f6564265faeac0d4f039ff487a8732ef50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd33ddf92389377dfce84044372db7ca3455f59c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
